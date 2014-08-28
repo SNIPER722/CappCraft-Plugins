ed0f7696550ced761eb9c1794e48de830eac87d7
Harry <tom8201@hotmail.com>
2014-08-28 18:29:07 -0400

    Add main & plugin.yml

---
+++ src/main/java/org/cappmc/sniper722/cappLogin/cappLogin.java
@@ -0,0 +1,10 @@
+package org.cappmc.sniper722.cappLogin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Harry on 28/08/2014.
+ */
+public class cappLogin extends JavaPlugin{
+
+}
