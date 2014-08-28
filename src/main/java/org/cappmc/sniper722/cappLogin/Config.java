5753abbb55ed1233189aa91bb7262a96ed2bcc35
Harry <tom8201@hotmail.com>
2014-08-28 19:48:43 -0400

    add default config settings (raw). Finish the config creation

---
+++ src/main/java/org/cappmc/sniper722/cappLogin/Config.java
@@ -0,0 +1,47 @@
+package org.cappmc.sniper722.cappLogin;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * Created by SNIPER722 on 28/08/2014.
+ */
+public class Config {
+    public static YamlConfiguration Config;
+
+    public static void SetDeafult(String path,Object value){
+        Config.set(path,Config.get(path,value));
+    }
+
+    public static YamlConfiguration load(boolean exsit){
+        String dir = "plugins/cappLogin";
+        File Settings = new File(dir + "config.yml");
+        try{
+            Config = new YamlConfiguration();
+            if (exsit){
+                Config.load(Settings);
+            }
+            SetDeafult("General.toggle",true);
+            SetDeafult("General.disconnectMessage","$1{player}, You use the wrong launcher!");
+            SetDeafult("SQL.url","127.0.0.1");
+            SetDeafult("SQL.port","3306");
+            SetDeafult("SQL.user","root");
+            SetDeafult("SQL.password","password");
+
+            //case new config
+            return Config;
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }catch(InvalidConfigurationException e){
+            e.printStackTrace();
+        }
+        // case error
+        return null;
+    }
+}
