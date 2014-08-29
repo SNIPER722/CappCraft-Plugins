6665827557764f24eb0f784424af6165bf62839a
Harry <tom8201@hotmail.com>
2014-08-29 10:30:58 -0400

    Add SQL support

--- src/main/java/org/cappmc/sniper722/cappLogin/Utils.java
+++ src/main/java/org/cappmc/sniper722/cappLogin/Utils.java
@@ -2,15 +2,22 @@ package org.cappmc.sniper722.cappLogin;
 
 import org.bukkit.entity.Player;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+
 /**
  * Created by SNIPER722 on 29/08/2014.
  */
 public class Utils {
 
     public static String getTime(){
-        String time = "2014-08-29";
-        //TODO: return a String format of MySQL Time format yyyy-mm-dd hh:mm:ss
-        return time;
+        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-M-dd hh:mm:ss");
+        return dateFormat.format(date);
     }
 
     public static boolean isVisitor(Player player){
@@ -20,5 +27,21 @@ public class Utils {
             result = false;
         }
         return result;// true for visitor false else
+    }
+
+    public static Connection sqlConnection(){
+        Connection conn = null;
+        String host = cappLogin.Settings.getString("SQL.url");
+        int port = cappLogin.Settings.getInt("SQL.port");
+        String table = cappLogin.Settings.getString("SQL.table");
+        String user = cappLogin.Settings.getString("SQL.user");
+        String password = cappLogin.Settings.getString("SQL.password");
+        try{
+           conn =  DriverManager.getConnection("jdbc:mysql://" + host + ";" + port + "/" + table, user, password);
+        }catch(SQLException e){
+            cappLogin.log.log(Level.WARNING,"Fail to connect to Database",e);
+        }
+
+        return conn;
     }
 }
