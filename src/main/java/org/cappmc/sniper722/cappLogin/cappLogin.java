30988f5f58d745bb5eb490f7e8070a435e11e96b
Harry <tom8201@hotmail.com>
2014-08-29 11:01:41 -0400

    add Enable

--- src/main/java/org/cappmc/sniper722/cappLogin/cappLogin.java
+++ src/main/java/org/cappmc/sniper722/cappLogin/cappLogin.java
@@ -1,9 +1,12 @@
 package org.cappmc.sniper722.cappLogin;
 
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -14,10 +17,33 @@ public class cappLogin extends JavaPlugin{
     static String maindir = "plugins/cappLogin/";
     public static YamlConfiguration Settings;
     static File SettingsFile = new File(maindir + "config.yml");
+    boolean enable = true;
 
     public void onEnable(){
+        new File(maindir).mkdir();
+        if (!SettingsFile.exists()){
+            try{
+                SettingsFile.createNewFile();
+                Settings = Config.load(true);
+            }catch (IOException e){
+                log.log(Level.WARNING,"Error loading config",e);
+            }
+        }else{
+            Settings = Config.load(false);
+        }
+        // End of loading Settings
+
+        enable = !Settings.getBoolean("Main.killswitch");
+        // register plugins manager
+        PluginManager pm = getServer().getPluginManager();
+        // Registers Listener
+        pm.registerEvents(new playerListener(), this);
+
+        log.log(Level.INFO,"[cappLogin] cappLogin is loaded");
+
 
     }
+
 
     public void onDisable(){
         Settings = null;
