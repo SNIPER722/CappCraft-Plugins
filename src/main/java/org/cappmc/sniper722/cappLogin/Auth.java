b5e9f11edcd01c9a1524a9071c34824779fc5b9e
Harry <tom8201@hotmail.com>
2014-08-28 21:45:41 -0400

    auth file waiting for Lu

---
+++ src/main/java/org/cappmc/sniper722/cappLogin/Auth.java
@@ -0,0 +1,13 @@
+package org.cappmc.sniper722.cappLogin;
+
+/**
+ * Created by SNIPEr722 on 28/08/2014.
+ */
+public class Auth {
+
+    public boolean allowJoin(String name){
+        boolean result = false;
+        //todo: waiting for lu.....
+        return result;
+    }
+}
