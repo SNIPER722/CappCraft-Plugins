0a3d7fb7df38546f77a7e0796e15593c07a7e1c0
Harry <tom8201@hotmail.com>
2014-08-28 21:40:12 -0400

    Logger

--- src/main/java/org/cappmc/sniper722/cappLogin/cappLogin.java
+++ src/main/java/org/cappmc/sniper722/cappLogin/cappLogin.java
@@ -2,9 +2,15 @@ package org.cappmc.sniper722.cappLogin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 /**
  * Created by Harry on 28/08/2014.
  */
 public class cappLogin extends JavaPlugin{
+    public static final Logger log = Logger.getLogger("Minecraft");
+
+
+
 
 }
