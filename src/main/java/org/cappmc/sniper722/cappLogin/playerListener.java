4f7fd56c5131e2067173bd3f355bea666cf9c236
Harry <tom8201@hotmail.com>
2014-09-17 09:49:32 -0400

    cleanup listener

--- src/main/java/org/cappmc/sniper722/cappLogin/playerListener.java
+++ src/main/java/org/cappmc/sniper722/cappLogin/playerListener.java
@@ -16,27 +16,8 @@ import java.util.logging.Level;
  * Created by SNIPER722 on 28/08/2014.
  */
 public class playerListener implements Listener{
-    /*@EventHandler(priority = EventPriority.HIGH)
-    public void onPlayerLogin(PlayerPreLoginEvent event){
-        if (cappLogin.Settings.getBoolean("General.forceCappLauncher")){
-            String playerName = event.getName();
-            boolean result = false;
-            try {
-                result = Auth.allowJoin(playerName);
-            }catch(IOException e){
-                cappLogin.log.log(Level.WARNING,"Error connect to Auth Server");
-                e.printStackTrace();
-            }
-            if (result){
-                String disconnectMessage = cappLogin.Settings.getString("General.disconnectMessage").replace("{player}",playerName);
-                event.setKickMessage(disconnectMessage);
-                event.setResult(PlayerPreLoginEvent.Result.KICK_OTHER);
-                cappLogin.log.log(Level.INFO," ["+playerName+"]("+event.getAddress().getHostAddress()+") USE Wrong Launcher!");
-            }
-        }
-    }*/
-    /*
-    public void onPlayerExit(PlayerQuitEvent event){
+
+    /*public void onPlayerExit(PlayerQuitEvent event){
         if (cappLogin.logs) {
             Utils.playerLog(event.getPlayer(),false);
         }
@@ -61,7 +42,7 @@ public class playerListener implements Listener{
                 serverFail = true;
                 e.printStackTrace();
             }
-            if (result){
+            if (!result){
                 if (serverFail) {
                     disconnectMessage = "Auth Server fail Please contact Admin!";
                 }else{
